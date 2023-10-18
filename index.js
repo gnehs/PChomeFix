2946a90d7dd906ef66d5289254ad820a6c4d4a6e
gnehs <jayuiop@gmail.com>
2023-10-18 14:28:22 +0800

    added --no-sandbox args

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function getBrowser() {
   if (!_browser) {
     _browser = await puppeteer.launch({
       headless: "new",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
   }
   return _browser;
